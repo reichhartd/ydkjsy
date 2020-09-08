064551a383defe57075d3a1dd43e172407df1614
Daniel Reichhart <daniel.reichhart@gmx.net>
2020-09-08 16:40:21 +0200

    Pillar 1

---
+++ src/getStarted/ch4pi1.js
@@ -0,0 +1,30 @@
+function range(start, end) {
+    function generateRange(endPoint) {
+        const iterations = endPoint - start + 1;
+        if (iterations === 1) {
+            return [start];
+        } else if (iterations < 1) {
+            return [];
+        }
+        const rangeArray = [...Array(iterations).keys()];
+        return rangeArray.map((number) => number + start);
+    }
+
+    if (end === undefined) {
+        return generateRange;
+    } else {
+        return generateRange(end);
+    }
+}
+
+console.log('[3]', range(3, 3));
+console.log('[3,4,5,6,7,8]', range(3, 8));
+console.log('[]', range(3, 0));
+
+var start3 = range(3);
+var start4 = range(4);
+
+console.log('[3]', start3(3));
+console.log('[3,4,5,6,7,8]', start3(8));
+console.log('[]', start3(0));
+console.log('[4,5,6]', start4(6));
